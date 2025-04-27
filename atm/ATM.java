c93b4ba850e8ab8d818be881ed0a9e71d560bfad
uday1683 <gembaliudaykrishna@gmail.com>
2025-04-28 04:37:48 +0530

    Add files via upload

---
+++ atm/ATM.java
@@ -0,0 +1,8 @@
+package atm;
+
+public class ATM extends OptionMenu {
+    public static void main(String[] args) {
+        OptionMenu options = new OptionMenu();
+        options.getLogin();
+    }
+}
